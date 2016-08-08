9532d96cf31fcd02f5cf84917f60cecdf8e0377f
Victor Rykov <soth1985@yahoo.com>
2016-08-08 19:02:44 +0700

    finish  tutorial

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,22 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour 
 {
 	public float speed = 0.1f;
+	public Text countText;
+	public Text winText;
 
 	private Rigidbody2D rb2d;
+	private int count;
 	// Use this for initialization
 	void Start () 
 	{
 		rb2d = GetComponent<Rigidbody2D> ();
+		count = 0;
+		winText.text = "";
+		SetCountText ();
 	}
 	
 	// Update is called once per frame
@@ -26,5 +33,23 @@ public class PlayerController : MonoBehaviour
 
 		//Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
 		rb2d.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter2D(Collider2D other) 
+	{
+		if (other.gameObject.CompareTag("PickUp"))
+		{
+			other.gameObject.SetActive(false);
+			++count;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString ();
+
+		if (count >= 12)
+			winText.text = "You win!";
 	}
 }
